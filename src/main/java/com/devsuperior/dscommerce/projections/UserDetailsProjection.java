3b92b3f98069d15cd5dc878812e02eb9987847f9
LUIZGCAVALCANTE <luizgustavolg@live.com>
2026-09-10 23:19:07 -0300

    adicionando projections

---
+++ src/main/java/com/devsuperior/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
+
+
+
+
+
+
