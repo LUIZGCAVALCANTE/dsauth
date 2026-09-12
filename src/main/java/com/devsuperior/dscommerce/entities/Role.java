fd06fa3f185c6fb9d8515465e69a0c2da1c95899
LUIZGCAVALCANTE <luizgustavolg@live.com>
2026-09-12 13:32:13 -0300

    adicionado e aperfeiçocando as configs security

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
-import java.util.Objects;
+import jakarta.persistence.*;
 
+import java.util.Objects;
+@Entity
+@Table( name= "tb_role")
 public class Role {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
