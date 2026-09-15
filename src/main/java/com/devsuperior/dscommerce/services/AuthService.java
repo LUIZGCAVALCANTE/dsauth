fe33d1f1d8ed709df36229da64c01d2cff174933
LUIZGCAVALCANTE <luizgustavolg@live.com>
2026-09-14 22:08:00 -0300

    adicionando, dtos restantes para orders e itens

---
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserService userService;
+
+    public void validateSelfOrAdmin(long userId) {
+        User me = userService.authenticated();
+        if (!me.hasRole("ROLE_ADMIN") && !me.getId().equals(userId)) {
+            throw new UsernameNotFoundException("Access denied - voce nao tem autorizacao");
+        }
+    }
+}
---
+++ src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dscommerce.services;
+
+
+
+
+import com.devsuperior.dscommerce.dto.OrderDTO;
+import com.devsuperior.dscommerce.dto.OrderItemDTO;
+import com.devsuperior.dscommerce.entities.*;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
+import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+
+
+
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository ordRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
+    public OrderDTO findById(Long id) {
+        Order order = ordRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("Order nao existente"));
+        authService.validateSelfOrAdmin(order.getClient().getId());
+        return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO dto) {
+
+        Order order = new Order();
+
+        order.setMoment(Instant.now());
+        order.setStatus(OrderStatus.WAITING_PAYMENT);
+
+        User user = userService.authenticated();
+        order.setClient(user);
+
+        for (OrderItemDTO itemDto : dto.getItems()) {
+            Product product = productRepository.getReferenceById(itemDto.getProductId());
+            OrderItem item = new OrderItem(order, product, itemDto.getQuantity(), product.getPrice());
+            order.getItems().add(item);
+        }
+
+        ordRepository.save(order);
+        orderItemRepository.saveAll(order.getItems());
+
+        return new OrderDTO(order);
+    }
+}
